aed48d00200bb2e50719e17463ee3d0fb6d8fbf8
Joshua Eze <ezejoshuac@gmail.com>
2023-08-01 16:38:58 +0100

    Added claims generation

---
+++ AuthImplementation.Services/JWT/IJwtAuthenticate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AuthImplementation.Model.Dtos.Response;
+using AuthImplementation.Model.Entities;
+
+namespace AuthImplementation.Services.JWT
+{
+    public interface IJwtAuthenticate
+    {
+        Task<JwtToken> GenerateTokenAsync(User user, string expiration = null, IList<Claim> claims = null);
+
+    }
+}
---
+++ AuthImplementation.Services/JWT/JwtAuthenticate.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using AuthImplementation.Model.Dtos.Response;
+using AuthImplementation.Model.Entities;
+using AuthImplementation.Model.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthImplementation.Services.JWT
+{
+    public class JwtAuthenticate : IJwtAuthenticate
+    {
+        public async Task<JwtToken> GenerateTokenAsync(User user, string expiration = null, IList<Claim> claims = null)
+        {
+
+        }
+
+        private async Task<IList<Claim>> GetClaimsAsync(User user, IList<Claim> claims = null)
+        {
+            IdentityOptions options = new();
+            var claimsBuilder = new List<Claim>
+            {
+                new Claim("Id", user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(options.ClaimsIdentity.UserIdClaimType, user.Id),
+                new Claim(options.ClaimsIdentity.UserNameClaimType, user.UserName),
+                new Claim(ClaimTypes.Role, user.UserTypeId.ToStringValue())
+            };
+            if (claims != null)
+                claimsBuilder.AddRange(claims);
+
+            return claimsBuilder;
+        }
+    }
+}
