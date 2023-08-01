d0ede7afa085cba0fe1d4a63a58dfdc74442f3e7
Joshua Eze <ezejoshuac@gmail.com>
2023-08-01 16:56:16 +0100

    Setup jwt properties in app settings
    - Create auth response dto

---
+++ AuthImplementation.Model/Dtos/Response/AuthResponses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthImplementation.Model.Dtos.Response;
+
+public class AuthResponse
+{
+    public JwtToken JwtToken { get; set; }
+    public string UserId { get; set; }
+    public string FullName { get; set; }
+    public string UserType { get; set; }
+}
+
+public class JwtToken
+{
+    public string Token { get; set; }
+    public DateTime Issued { get; set; } = DateTime.Now;
+    public DateTime Expiration { get; set; }
+}
--- AuthImplementation/SeedData/Seed.cs
+++ AuthImplementation/SeedData/Seed.cs
@@ -11,7 +11,6 @@ public static class Seed
         ApplicationDbContext db = builder.ApplicationServices.CreateScope().ServiceProvider
             .GetRequiredService<ApplicationDbContext>();
 
-        db.Database.EnsureCreated();
         if (db.Roles.Any())
             return;
         await db.Roles.AddRangeAsync(SeedRoles());
