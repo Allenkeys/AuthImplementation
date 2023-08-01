25b543a1726a77dd7dbaeb3aebbd6c4780443ba6
Joshua Eze <ezejoshuac@gmail.com>
2023-08-01 15:32:26 +0100

    Seed roles

---
+++ AuthImplementation/SeedData/Seed.cs
@@ -0,0 +1,42 @@
+﻿using AuthImplementation.Data.Context;
+using AuthImplementation.Model.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthImplementation.SeedData;
+
+public static class Seed
+{
+    public static async Task SeedAll(this IApplicationBuilder builder)
+    {
+        ApplicationDbContext db = builder.ApplicationServices.CreateScope().ServiceProvider
+            .GetRequiredService<ApplicationDbContext>();
+
+        if (db.Roles.Any())
+            return;
+        await db.Roles.AddRangeAsync(SeedRoles());
+        await db.SaveChangesAsync();
+    }
+    public static IEnumerable<IdentityRole> SeedRoles()
+    {
+        return new List<IdentityRole>()
+        {
+            new IdentityRole()
+            {
+                Name = UserType.Admin.ToStringValue(),
+                NormalizedName = UserType.Admin.ToStringValue()!.ToUpper().Normalize()
+            },
+
+            new IdentityRole()
+            {
+                Name = UserType.BackOffice.ToStringValue(),
+                NormalizedName = UserType.BackOffice.ToStringValue()!.ToUpper().Normalize()
+            },
+
+            new IdentityRole()
+            {
+                Name = UserType.FrontOffice.ToStringValue(),
+                NormalizedName = UserType.FrontOffice.ToStringValue()!.ToUpper().Normalize()
+            }
+        };
+    }
+}
