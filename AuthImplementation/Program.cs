6100104f13acce721f9708efc2242b5a55201ef4
Joshua Eze <ezejoshuac@gmail.com>
2023-08-03 10:09:34 +0100

    One-time configuration of authorization middleware for backoffice and admin users.

--- AuthImplementation/Program.cs
+++ AuthImplementation/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.OpenApi.Models;
 using AuthImplementation.Extensions;
 using AuthImplementation.SeedData;
+using AuthImplementation.Model.Enums;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,20 @@ builder.Services.InjectServices();
 builder.Services.AddDbConnecion();
 builder.Services.ConfigureIdentity();
 builder.Services.ConfigureJWT(builder.Configuration);
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ExecutiveOnly", policy =>
+    {
+        policy.RequireRole
+        (
+            UserType.BackOffice.ToStringValue(),
+            UserType.Admin.ToStringValue()
+        );
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
